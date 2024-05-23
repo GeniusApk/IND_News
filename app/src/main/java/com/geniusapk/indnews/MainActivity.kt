b0bddd3f6c7b9dad5d3f0da898d15b27f21bc242
GeniusApk <kaakib7785@gmail.com>
2024-05-23 15:09:26 +0530

    update tab

--- app/src/main/java/com/geniusapk/indnews/MainActivity.kt
+++ app/src/main/java/com/geniusapk/indnews/MainActivity.kt
@@ -1,47 +1,15 @@
 package com.geniusapk.indnews
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.geniusapk.indnews.network.ApiProvider
-import com.geniusapk.indnews.screen.AnimatedShimmer
-import com.geniusapk.indnews.screen.HomeApp
-import com.geniusapk.indnews.screen.NewsTabs
 import com.geniusapk.indnews.screen.NewsViewModel
 import com.geniusapk.indnews.ui.theme.INDNewsTheme
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import mainui
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/geniusapk/indnews/screen/tabBar.kt
+++ app/src/main/java/com/geniusapk/indnews/screen/tabBar.kt
@@ -1,20 +1,27 @@
 package com.geniusapk.indnews.screen
 
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NewsTabs(viewModel: NewsViewModel) {
-    val tabs = listOf("General", "Tech", "Sports" , "Business")
+    val tabs = listOf("General", "Tech", "Sports" , "Business" , "Entertainment" , "Science")
     var selectedTabIndex by remember { mutableStateOf(0) }
 
-    TabRow(selectedTabIndex = selectedTabIndex,
+    ScrollableTabRow(selectedTabIndex = selectedTabIndex,
+        //modifier = Modifier.fillMaxWidth().horizontalScroll(rememberScrollState()),
+        edgePadding = 16.dp,
 
 
         ) {
@@ -28,6 +35,8 @@ fun NewsTabs(viewModel: NewsViewModel) {
                         "Tech" -> "technology"
                         "Sports" -> "sports"
                         "Business" -> "business"
+                        "Entertainment" -> "entertainment"
+                        "Science" -> "science"
                         else -> null
                     }
                     viewModel.updateCategory(category)
